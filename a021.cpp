74047ada3a2355f31721dd10d7764fdb35d97d65
Calvin-Zeng <cilun0812@gmail.com>
2015-07-17 18:38:04 +0800

    增加除法運算部分，但第一位>第二位的除法還沒寫

--- a021.cpp
+++ a021.cpp
@@ -45,7 +45,6 @@ string add(char status, string intput_1, string intput_2) { //加/減/乘法運算
 	string output = ("");
 	int n1, n2;
 	int bit, sum;
-	int tmp_sub[500];
 	char tmp1, tmp2;
 	//只有在加法運算時才判斷是否兩數位元是否一致
 	if (status == '+' || status == '-') {
@@ -99,23 +98,21 @@ string add(char status, string intput_1, string intput_2) { //加/減/乘法運算
 			}
 			else {
 				borrow(x - 1, &intput_1);
-				sum = tmp1 + '0' - tmp2 - '0' + sub_10;
+				sum = (tmp1 - '0') - (tmp2 - '0') + sub_10;
 			}
 
 			output.insert(0, int2str(sum));
 			sum = 0;
-
+			int count = 0;
 			if (x == 0) {
 				while (1) {
-					int count = 0;
-					count++;
-					if (output[0] == '0') {
-						output = output.assign(output, count, output.size());
-
-					}
+					if (output[count] == '0')
+						count++;
 					else
 						break;
 				}
+				if(count!=0)
+					output = output.assign(output, count, output.size());
 				if (output.empty())
 					output = "0";
 			}
@@ -151,7 +148,7 @@ string mult(string intput_1, string intput_2) { //乘法運算位元移動，再使用加法。
 		if (y != 0)
 			supplement(1, intput_1.size(), &intput_1);  //將第1個數的第1bit補0
 		tmp = tmp.assign(intput_2, intput_2.size() - y - 1, intput_2.size() - y);  //將第2個數的第1bit刪除
-																				   //cout <<"值:"<< tmp<<"-"<<intput_1<<endl;
+		//cout <<"值:"<< tmp<<"-"<<intput_1<<endl;
 		output = add('+', output, add('*', intput_1, tmp));     //使用加法完成乘法運算
 	}
 	return output;
@@ -175,18 +172,108 @@ int bit_size_swap(int x, string *intput_1, string *intput_2) {  //輸入兩數，把較
 	}
 	return 0;
 }
+int comparison_of_bit_size(string intput_1, string intput_2) {
+	if (intput_1.size()>intput_2.size()){
+		return 1;
+	}
+	else if (intput_1.size()<intput_2.size()){
+		return 2;
+	}
+	else {
+		for (int i = 0; i < intput_1.size(); i++){
+			if ((intput_1[i] - '0')>(intput_2[i] - '0')){
+				return 1;
+			}
+			else if ((intput_1[i] - '0') < (intput_2[i] - '0')) {
+				return 2;
+			}
+			else {
+				if (i== intput_1.size()-1){
+					return 0;
+				}
+			}
+		}
+	}
 
-// string exc(string intput_1,string intput_2){
-// string output=("");
-// int bit=intput_1.size()-intput_2.size();
-// if(bit==0){
-// if(intput_1[0]-'0'>intput_1[0]-'0'){
-// }
-// }else{
-// supplement(bit,intput_2);
-// }
-// return output;
-// }
+}
+string exc(string intput_1,string intput_2){
+	string output=("");
+	string tmp;
+	char tmp1, tmp2;
+	int bit = intput_1.size() - intput_2.size();
+	if (comparison_of_bit_size(intput_1, intput_2)==1)
+	{
+		if (bit == 0) {
+			for (int i = 9; i > 0; i--) {
+				if (i == 1) {
+					tmp = add('-', intput_1, intput_2);
+					bit = tmp.size();
+					if (tmp[0] == '0')
+						return "1";
+					if (bit<intput_2.size())
+						return "1";
+					if (bit == intput_2.size() && (intput_2[0] - '0')>(tmp[0] - '0'))
+						return "1";
+				}
+				else {
+					tmp = mult(intput_2, int2str(i));
+					if (comparison_of_bit_size(tmp, intput_1)==0)
+						return int2str(i);
+					else if (comparison_of_bit_size(tmp, intput_1) == 1)
+						continue;
+					else if (comparison_of_bit_size(tmp, intput_1) == 2)
+						return int2str(i);
+
+				}
+			}
+		}
+		else {
+			/*下次撰寫處*/
+		}
+	}
+	else if (comparison_of_bit_size(intput_1, intput_2) == 2)
+	{
+		return "0";
+	}
+	else {
+		return "1";
+	}
+
+	//if(bit >0){
+	//	supplement(bit, intput_2.size(),&intput_2);
+	//	for (int i = 0; i < intput_2.size(); i++)
+	//	{
+	//		if ((intput_2[i]-'0')>(intput_1[i]-'0'))
+	//		{
+	//			intput_2 = intput_2.assign(intput_2, 0, intput_2.size()-2);
+	//			bit--;
+	//			break;
+	//		}
+	//	}
+	//	for (int i = 0; i <bit; i++)
+	//	{
+	//		for (int y = 9; y > 0; y--)
+	//		{
+	//			tmp=mult(intput_2, int2str(y));
+	//			if (tmp.size() > intput_1.size())
+	//				continue;
+	//			if (tmp.size() == intput_1.size())
+	//			{
+	//				for (int x = 0; x < tmp.size(); x++)
+	//				{
+
+	//				}
+	//			}
+
+	//		}
+	//		intput_2 = intput_2.assign(intput_2, 0, intput_2.size() - 2);
+	//	}
+	//}else if (bit <0)
+	//{
+	//	return "0";
+	//}
+ //return output;
+ }
 
 int main() {
 	string intput;
@@ -197,9 +284,10 @@ int main() {
 		intput_1 = intput_1.assign(intput, 0, find);
 		intput_2 = intput_2.assign(intput, find + 3, intput.size());
 		//cout <<intput_1<<"  "<<intput_2<<endl;
-		bit = intput_1.size() - intput_2.size();
-		bit = bit_size_swap(bit, &intput_1, &intput_2);
 		intput = intput[find + 1];
+		if(intput[0]!='/')
+			bit = bit_size_swap(intput_1.size() - intput_2.size(), &intput_1, &intput_2);
+		
 		//cout <<intput[0]<<"\n-----------"<<endl;
 		switch (intput[0]) {
 		case '*':
@@ -214,9 +302,13 @@ int main() {
 			else
 				cout << add('-', intput_1, intput_2) << endl;
 			break;
+		case '/':
+			cout << exc(intput_1, intput_2) << endl;
+			break;
 		}
 		if (cin.eof())
 			break;
 	}
+	//system("PAUSE");
 	return 0;
 }

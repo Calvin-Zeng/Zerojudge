a4dfabcd87fdb67e8cc7d735e9a288cbb6252079
Calvin-Zeng <cilun0812@gmail.com>
2015-07-18 16:52:14 +0800

    2390110 cilun0812 a042. 平面圓形切割 AC (8ms, 120KB)

---
+++ a042.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int input1;
+	string tmp;
+	while (cin >> input1)
+	{
+		cout << input1*input1 - input1 + 2 << endl;
+	}
+	//system("PAUSE");
+	return 0;
+}

401399439fbfae954cc70d871f076a94ab237a9c
Calvin-Zeng <cilun0812@gmail.com>
2015-07-19 16:30:46 +0800

    2391484 cilun0812 a121. 質數又來囉 AC (68ms, 176KB) - 稍微增加速度

--- a121.cpp
+++ a121.cpp
@@ -34,7 +34,10 @@ int main() {
 		count = 0;
 		for (int i = intput1; i <= intput2; i++)
 		{
+			if (i != 2 && i != 3 && i % 2 == 0 && i % 3 == 0)
+				continue;
 			count += is_prime(i);
+
 		}
 		cout << count<<endl;
 	}
